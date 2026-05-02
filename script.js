58cd9718815a20cb961021012574cc7f7dcb084e
Prince Gupta <prince177614@gmail.com>
2026-05-02 21:03:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//variable type
+// var-> after assigned value, you any update/change.
+
+var MeraName = "Prince";
+
+MeraName = "Prannu";
+console.log("Prince")
+console.log(typeof(MeraName))
+
+// const-> once value is assigned, then it will not update/change.
+
+const Name = "Pranshu";
+console.log("Gravity Coding")
+console.log(typeof(Name))
+
+const num = 1202;
+console.log(typeof(num))
+
+
+// Name = "Pranu";
+
+//let-> scope
+
+let Fruit = "Apple";
+console.log("fruit")
+console.log(typeof(Fruit))
+
+
+//console -> method
+// console.log("Prince")
+// console.log(typeof(MeraName))
